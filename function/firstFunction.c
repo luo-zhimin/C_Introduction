f58fd786f231828ef1cdb48f047759f09e4b49a8
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-03-12 00:12:04 +0800

    recursion/scope/static function

--- function/firstFunction.c
+++ function/firstFunction.c
@@ -3,6 +3,11 @@
 //用户头文件
 #include "./fisrtFunction.h"
 
+//有返回值-自动转换
+double getSum(int a,int b){
+    return a+b;//自动转换
+}
+
 int main() {
 
     /*
@@ -14,7 +19,9 @@ int main() {
 
           形参列表：表示函数的输入
           函数中的语句：表示为了实现某一功能代码块
-          函数可以有返回值,也可以没有, 如果没有返回值，返回类型 声明为 void
+          函数可以有返回值,也可以没有
+            如果没有返回值，返回类型 声明为 void
+            如果有返回值，返回值类型要明确，要求返回值和返回类型匹配，或者可以进行相互转换(自动/强制转换)[显示/隐示]
      */
     printf("function");
     //函数封装
@@ -22,6 +29,10 @@ int main() {
     printf("\nadd %.2f",add);
 
     sayName();
+
+    //同一类中 顺序执行 从上到下～
+    double result = getSum(9,1);
+    printf("\ngetSum result [%.2f]",result);
 
     //传统方法(计算多次 会有多个传统方法产生)
 //    switch (operation) {
@@ -49,3 +60,4 @@ int main() {
 void sayName(){
     printf("\nclion second function~");
 }
+
